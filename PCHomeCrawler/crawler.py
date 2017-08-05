2ecde7538ad892d8b43ad7a1a1924a9af36f6dd9
Lin Brandon <s006504504@gmail.com>
2017-08-06 00:32:20 +0800

    add hotNewRankCrawler

--- PCHomeCrawler/crawler.py
+++ PCHomeCrawler/crawler.py
@@ -3,16 +3,12 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import re
-import sys
 import json
 import requests
 import argparse
 import time
-import codecs
-from selenium import webdriver
+import codecs,sys
 from bs4 import BeautifulSoup
-from six import u
-from selenium import webdriver
 import time
 
 baseUrl="https://tw.buy.yahoo.com/"
@@ -28,44 +24,106 @@ if sys.version_info[0] < 3:
     requests.packages.urllib3.disable_warnings()
 
 class PCHomeCrawler(object):
-    def __init__(self):
+    def __init__(self,cmdline=None):
         ##request~
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+            Crawler for buy.yahoo.tw  
+            Input: Type
+            Output: To Parse DB
+        ''')
+        parser.add_argument('-type', metavar='CrawlerType', help='TYPE hotranknew/subs/list ', required=True)
+        parser.add_argument('-index', metavar=('START_INDEX', 'END_INDEX'), type=int, nargs=2, help="Start and end index for zones")
+        parser.add_argument('-list', metavar='List', help="List all zones and subs")
+        
+        if cmdline:
+            args = parser.parse_args(cmdline)
+        else:
+            args = parser.parse_args()
+
         url="https://tw.buy.yahoo.com/"
         res = requests.get(baseUrl)
         new_text=re.sub(r"(\n\s*<!--\n\s*<)|(\n\s*-->\n\s*<)","\n<",res.text)
         #print(new_text)
         soup=BeautifulSoup(new_text, "html.parser")
-        zoneResultList,sidList, zidList= [],[],[]
+        zoneResultList,sidList, zidList,subsToZone = [], [], [],{}
         zonelist=soup.select("li[zid]")
         for zone in zonelist:
-            sublist=zone.select("li[sid]")
             subs=[]
-            for sub in sublist:
+            for sub in zone.select("li[sid]"):
                 sidList.append(int(sub['sid']))
                 subs.append({"subname":sub.get_text().replace('\n',''),"sid":sub['sid']})
+                subsToZone[int(sub['sid'])]={'zid':zone['zid'],'zone':zone.select("a.yui3-menu-label")[0].text}
             zoneResultList.append({"zone":zone.select("a.yui3-menu-label")[0].text,"subs":subs,"zid":zone['zid']})
             zidList.append(zone['zid'])
-        #print(zoneResultList)
-        print(len(zonelist))
-        print(zidList)
-        print(sorted(sidList))
-        listnum=0
-        for zone in zoneResultList:
-            print('\n'+zone["zone"])
-            zoneUrl=baseUrl+"?z="+zone['zid'].split('z')[1]
-            self.crawlData(self,zoneUrl,'zone',zone['zone'],zone['zid'])
-            for sub in zone['subs']:
-                print(listnum)
-                listnum+=1
-                subUrl=baseUrl+"?sub="+sub['sid']
-                print('\n'+sub["subname"])
-                self.crawlData(self,subUrl,'sub',zone['zone'],zone['zid'],sub["subname"],int(sub['sid']))
-            time.sleep(0.05)
+        if args.type=="list" :
+            print('Zone Number:',len(zonelist),'\n','ZoneIDs',zidList,'\n','SubIDs',sorted(sidList))
+        elif args.type=="hotranknew":
+            timestamp = int(time.time())
+            #first time for get the valid subs those have ranking data
+            requestsAPIBase=baseUrl+"catalog/ajax/recmdHotNew?segmentId=999999&t="+str(timestamp)+"&subId="
+            r=requests.get(requestsAPIBase+json.dumps(sorted(sidList)).replace('[','').replace(']',''))
+            rs=json.loads(r.text)['billboard']
+            validSubIdList=rs['subId'].split(",")
+            for smallSublist in [validSubIdList[i:i+10] for i in range(0, len(validSubIdList), 10)]:
+                urlString=json.dumps(smallSublist).replace('[','').replace(']','').replace('"','')
+                r=requests.get(requestsAPIBase+urlString)
+                rs=json.loads(r.text)['billboard']
+                labels,smallSubIds=[item['label'] for item in rs['tabs']+rs['othertab']],rs['subId'].split(",")
+                for idx,subitems in enumerate(rs['panels']):
+                    subid,subname=int(smallSubIds[idx]),labels[idx]
+                    zoneName,zid=subsToZone[subid]['zone'],subsToZone[subid]['zid']
+                    self.crawlHotRankNewItem(self,subitems['mainitem'],zoneName,zid,subname,subid)
+                    for item in subitems['pditem']:
+                        self.crawlHotRankNewItem(self,item,zoneName,zid,subname,subid)
+        elif args.type=="subs":
+            print("subs")
+            if args.index and args.index[0]>=0:
+                start = args.index[0]
+                end = start if args.index[1]<start else args.index[1]
+            else:
+                start,end=0,len(zidList)-1
+            print(start,end)
+            listnum=0
+            for idx,zone in enumerate(zoneResultList):
+                if idx<start:
+                    continue
+                if idx>=end:
+                    break
+                print('\n'+zone["zone"])
+                zoneUrl=baseUrl+"?z="+zone['zid'].split('z')[1]
+                self.crawlData(self,zoneUrl,'zone',zone['zone'],zone['zid'])
+                for sub in zone['subs']:
+                    print(listnum)
+                    listnum+=1
+                    subUrl=baseUrl+"?sub="+sub['sid']
+                    print('\n'+sub["subname"])
+                    self.crawlData(self,subUrl,'sub',zone['zone'],zone['zid'],sub["subname"],int(sub['sid']))
+        else:
+            print('you have the wrong type')    
+        
+    @staticmethod
+    def crawlHotRankNewItem(self,pditem,zoneName,zid,subName,subId):
+        price =BeautifulSoup(pditem['price'], "lxml").get_text().replace('$','')
+        data={
+            "picHref" : pditem['pdimg'],
+            "zoneName":zoneName,
+            "zoneId":zid,
+            "pid":pditem['pdid'],
+            "itemName":pditem['desc'],
+            "url":pditem['url'],
+            "price":price,
+            "detailCrawled":False,
+            "rank":int(pditem['hpp'].split('item')[1]),
+            "subId":subId,
+            "subName":subName
+        }
+        print(pditem['desc'],self.saveItem("NewHotItem",data))
+        return 0
     @staticmethod
     def saveItem(saveClass,data):
         url = 'https://parseapi.back4app.com/classes/'+saveClass 
         r = requests.post(url, data=json.dumps(data), headers=parseServerheaders)
-        time.sleep(0.01)
+        time.sleep(0.005)
         return r.text
     @staticmethod
     def crawlData(self,url,classType,zoneName,zid,subName=0,subId=0):
@@ -83,7 +141,7 @@ class PCHomeCrawler(object):
                       "zoneId":zid,
                       "itemName":intro.select('.text')[0].text,
                       "url":intro.select('.text')[0].a['href'],
-                      "price":int(intro.select('.red-price')[0].text),
+                      "price":intro.select('.red-price')[0].text ,
                       "detailCrawled":False,
                       "rank":idx+1
                   }
@@ -91,7 +149,7 @@ class PCHomeCrawler(object):
             if subId>0:
                 data['subId']=subId
                 data['subName']=subName
-            print(json.dumps(data))
+            #print(json.dumps(data))
             
             print(self.saveItem(classType+"Item",data))
 
